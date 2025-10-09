075f621026b5a8d837cb620464e5a00f58008cdf
Rae Bonfanti <rae.bonfanti@dot.ca.gov>
2025-10-08 17:06:20 -0700

    Force login user for Playwright
    Signed-off-by: Doc Ritezel <doc.ritezel@dot.ca.gov>

--- src/cal_bc/projects/urls.py
+++ src/cal_bc/projects/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.landings_index, name="index"),
+    path("", views.landings_index, name="landings"),
     path("projects/", views.projects_index, name="projects"),
 ]
--- tests/cal_bc/test_projects.py
+++ tests/cal_bc/test_projects.py
@@ -1,12 +1,17 @@
 import pytest
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from playwright.sync_api import Page
+from django.contrib.auth.models import User
 
 
 class TestProjects(StaticLiveServerTestCase):
     @pytest.fixture(autouse=True)
     def setup(self, page: Page):
         self.page = page
+        user = User.objects.create_user(username="caltrans")
+        self.client.force_login(user)
+        cookie = self.client.cookies['sessionid']
+        self.page.context.add_cookies([{'name': 'sessionid', 'value': cookie.value, 'secure': False, 'url': self.live_server_url}])
 
     def test_projects(self):
         self.page.goto(f"{self.live_server_url}/")
