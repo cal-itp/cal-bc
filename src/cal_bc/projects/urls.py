2287855dda3fb44d6ab9ed5befbfa2782f79b801
Doc Ritezel <pair+doc@ministryofvelocity.com>
2025-10-10 18:47:29 -0700

    Add admin user instructions and initial migration
    Signed-off-by: Rae Bonfanti <rae.bonfanti@dot.ca.gov>

--- src/cal_bc/projects/urls.py
+++ src/cal_bc/projects/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path("", views.landings_index, name="landings"),
     path("projects/", views.projects_index, name="projects"),
+    path(
+        "", TemplateView.as_view(template_name="landings/index.html"), name="landings"
+    ),
 ]
