24641627093125af8e014189d3ccdcf6e5ff8097
Doc Ritezel <doc@ministryofvelocity.com>
2025-10-22 10:30:34 -0700

    Analyst sees static files served properly (#58)
    * This fixes an issue where the site would not display properly due to CSS not being served
    
    Co-authored-by: Rae Bonfanti <rae.bonfanti@dot.ca.gov>

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,4 +4,5 @@ set -ex
 
 uv run --no-sync manage.py migrate
 uv run --no-sync manage.py tailwind build
+uv run --no-sync manage.py collectstatic
 uv run --no-sync manage.py runserver 0.0.0.0:8000
--- src/cal_bc/urls.py
+++ src/cal_bc/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import include, path, re_path
-from django.contrib.auth.views import LogoutView
 import azure_auth.views
+
+from django.conf import settings
+from django.contrib import admin
+from django.contrib.auth.views import LogoutView
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("azure_auth/", include("azure_auth.urls"), name="azure_auth"),
@@ -26,4 +29,5 @@ urlpatterns = [
     re_path(r"^admin/login/$", azure_auth.views.azure_auth_login),
     path("admin/", admin.site.urls),
     path("", include("cal_bc.projects.urls"), name="landing"),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
