d84b5cdb00060b3f1d1a62f4bd37b6b4b6b31f7e
Doc Ritezel <doc.ritezel@dot.ca.gov>
2026-07-14 10:16:54 -0700

    Developer can run migrations via the command line
    Signed-off-by: Erika Pacheco <erika.pacheco@dot.ca.gov>

---
+++ iac/jobs.tf
@@ -0,0 +1,51 @@
+resource "google_cloud_run_v2_job" "cal-bc-staging-manage" {
+  name                = "cal-bc-staging-manage"
+  location            = "us-west2"
+  deletion_protection = false
+
+  template {
+    template {
+      service_account = data.terraform_remote_state.iam.outputs.google_service_account_cal-bc-service-account_email
+      max_retries = 0
+
+      volumes {
+        name = "cloudsql"
+
+        cloud_sql_instance {
+          instances = [google_sql_database_instance.cal-bc-staging.connection_name]
+        }
+      }
+
+      containers {
+        image   = "us-west2-docker.pkg.dev/cal-itp-data-infra-staging/ghcr/cal-itp/cal-bc/cal-bc:${var.image_tag}"
+        command = ["uv", "run", "--no-sync", "manage.py"]
+        args    = ["help"]
+
+        volume_mounts {
+          name       = "cloudsql"
+          mount_path = "/cloudsql"
+        }
+
+        env {
+          name = "SECRET_KEY"
+          value_source {
+            secret_key_ref {
+              secret  = google_secret_manager_secret.cal-bc-staging-secret-key.secret_id
+              version = "latest"
+            }
+          }
+        }
+
+        env {
+          name = "DATABASE_URL"
+          value_source {
+            secret_key_ref {
+              secret  = google_secret_manager_secret.cal-bc-staging-database-url.secret_id
+              version = "latest"
+            }
+          }
+        }
+      }
+    }
+  }
+}
