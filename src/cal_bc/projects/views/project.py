c43cbb6aacd1893fed4f8a52c79890360d5809b9
Erika <erika@ministryofvelocity.com>
2026-07-16 01:20:02 -0300

    Add Delete link to Projects List

--- src/cal_bc/projects/views/project.py
+++ src/cal_bc/projects/views/project.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 from django.views.generic.base import RedirectView
 from django.shortcuts import get_object_or_404
 
 from cal_bc.projects.models.project import Project
 from cal_bc.models.models.model import Subsection
-
 
 class ProjectListView(LoginRequiredMixin, ListView):
     paginate_by = 10
@@ -31,3 +30,7 @@ class ProjectEditRedirectView(LoginRequiredMixin, RedirectView):
         except Subsection.DoesNotExist:
             raise Http404("No Subsection matches the given query.")
         return reverse_lazy("project_subsection_edit", kwargs={"project_pk": project.pk, "pk": subsection.pk})
+
+class ProjectDeleteView(LoginRequiredMixin, DeleteView):
+    model = Project
+    success_url = reverse_lazy('projects')
