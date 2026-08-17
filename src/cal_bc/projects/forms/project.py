3cbaa69e5dbc84500081dddca97c0362f0b08d2d
Erika <erika@ministryofvelocity.com>
2026-08-17 12:24:25 -0700

    Set range to a Field

--- src/cal_bc/projects/forms/project.py
+++ src/cal_bc/projects/forms/project.py
@@ -1,4 +1,5 @@
-from django.forms import CharField, ChoiceField, HiddenInput, ModelForm
+from django.core.exceptions import ValidationError
+from django.forms import CharField, ChoiceField, DecimalField, HiddenInput, ModelForm
 from django.utils.translation import gettext as _
 
 from cal_bc.projects.models.project import Value
@@ -18,17 +19,29 @@ class ValueForm(ModelForm):
             "field": HiddenInput(),
         }
 
+    def clean_value(self):
+        value = self.cleaned_data.get("value")
+        if value and hasattr(self.instance, "field") and hasattr(self.instance.field, "fieldrange") and not (self.instance.field.fieldrange.min_value <= float(value) <= self.instance.field.fieldrange.max_value):
+            raise ValidationError(f"Enter a number between {self.instance.field.fieldrange.min_value} and {self.instance.field.fieldrange.max_value}.")
+        return value
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if "initial" in kwargs:
             field = kwargs["initial"]["field"]
+
         if "instance" in kwargs:
             field = kwargs["instance"].field
+
         values = field.value_set.all()
+
         if len(values):
             self.fields["value"] = ChoiceField(
                 choices=[(None, ""), *[(v.value, v.name) for v in values]]
             )
+        elif hasattr(field, "fieldrange"):
+            self.fields["value"] = DecimalField()
         else:
             self.fields["value"] = CharField()
+
         self.fields["value"].label = field.name
