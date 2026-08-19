47e511c30e008fe9c3d26ef9179338ac654f2f54
Erika <erika@ministryofvelocity.com>
2026-08-18 17:58:13 -0700

    Admin sees project values

--- src/cal_bc/projects/admin.py
+++ src/cal_bc/projects/admin.py
@@ -4,10 +4,25 @@ from django.contrib import admin
 from django.db import transaction
 
 from ..tasks import refresh_channel
-from .models.project import Project
+from .models.project import Project, Value
+
+
+class ValueInline(admin.TabularInline):
+    model = Value
+    fields = ('value',)
+    readonly_fields=('value',)
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    inlines = [ValueInline]
+
     def save_model(self, request, obj, form, change):
         with transaction.atomic():
             super().save_model(request, obj, form, change)
