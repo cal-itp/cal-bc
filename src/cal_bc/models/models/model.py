8def5c5eba874fb404d01e2c5d6775867f7845a7
Rae Bonfanti <raebot@users.noreply.github.com>
2026-08-25 15:01:34 -0700

    Add index to Group is_summary

--- src/cal_bc/models/models/model.py
+++ src/cal_bc/models/models/model.py
@@ -106,7 +106,7 @@ class Group(models.Model):
     name = models.CharField(null=False, blank=False, db_index=True)
     description = models.CharField(blank=True)
     position = models.PositiveIntegerField(default=0, null=False, db_index=True)
-    is_summary = models.BooleanField(default=False)
+    is_summary = models.BooleanField(default=False, db_index=True)
 
     class Meta:
         ordering = ["position"]
