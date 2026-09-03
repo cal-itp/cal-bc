3c1df5d4e00355faac53e582566f1355180bce9e
Rae Bonfanti <raebot@users.noreply.github.com>
2026-09-02 19:15:41 -0700

    Add is_summary field to Group admin view

--- src/cal_bc/models/admin.py
+++ src/cal_bc/models/admin.py
@@ -107,7 +107,7 @@ class GroupAdmin(nested_admin.NestedModelAdmin):
     ordering = ["name"]
     search_fields = ["name", "subsection__code", "subsection__name", "subsection__code", "subsection__section__name", "subsection__section__version__name", "subsection__section__version__model__name"]
     search_help_text = "Search by Name, Model, Version, Section, and Subsection"
-    fields = ["model_name", "version_name", "section", "subsection", "name", "description"]
+    fields = ["model_name", "version_name", "section", "subsection", "name", "is_summary", "description"]
     readonly_fields = ["model_name", "version_name", "section"]
 
     @admin.display(description="Model", ordering="subsection__section__version__model__name")
